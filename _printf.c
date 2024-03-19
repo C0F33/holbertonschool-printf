066e5b8181dfd49645f97236c9d5f75735b0687e
angel bermudez <9015@holbertonstudents.com>
2024-03-19 19:42:11 -0400

    angel branch push 1

--- _printf.c
+++ _printf.c
@@ -1,49 +1,48 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "main.h"
+
 /**
- * _printf - prints anyy argument to stdout
+ * _printf - prints any argument to stdout
  * @format: the format string
- * Return: return 0 if success
-*/
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	va_list args;
+    va_list args;
+    int written_chars = 0;
 
-	int written_chars = 0;
+    va_start(args, format);
 
-	va_start(args, format);
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 's')
+            {
+                char *s = va_arg(args, char *);
+                while (*s != '\0')
+                {
+                    putchar(*s);
+                    s++;
+                    written_chars++;
+                }
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                written_chars++;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            written_chars++;
+        }
+        format++;
+    }
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				while (*s != '\0')
-				{
-					putchar(*s);
-					s++;
-					written_chars++;
-				}
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				written_chars++;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			written_chars++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (written_chars);
+    va_end(args);
+    return written_chars;
 }
-

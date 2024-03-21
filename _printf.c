7e200ede342ada12f18c37ea16834a23250f04d6
angel bermudez <9015@holbertonstudents.com>
2024-03-20 20:43:08 -0400

    changed approach for printf function, aryam suggests use a sort of nested function to streamline code length

--- _printf.c
+++ _printf.c
@@ -3,52 +3,30 @@
 #include "main.h"
 
 /**
- * _printf - prints any argument to stdout
+ * _printf -Function that prints any argument to stdout
  * @format: the format string
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int i = 0;
+	va_list ap;
+	int count = 0;
 
-    va_start(ap, format);
+	va_start(ap, format);
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 's')
-            {
-                char *s = va_arg(ap, char *);
-                while (*s != '\0')
-                {
-                    putchar(*s);
-                    s++;
-                    i++;
-                }
-            }
-            else if (*format == 'c')
-            {
-                char c = va_arg(ap, int);
-                putchar(c);
-                i++;
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                i++;
-            }
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			count += funct_print(ap, *format);
+		}
+		else
+		{
+            _putchar(*format);
         }
-        else
-        {
-            putchar(*format);
-            i++;
-        }
-        format++;
-    }
-
-    va_end(ap);
-    return (i);
+		format++;
+	}
+	va_end(ap);
+	return (count);
 }
